91c7deba2794ceb97b6eeaeb06b40cb2ecf1e0e6
Danilo Silva <danilo.silva@msn.com>
2023-03-10 15:31:32 -0300

    Criando e Enriquecendo o Modelo de Domínio

--- CleanArchitecture/CleanArchitecture.Domain/Entities/Categoria.cs
+++ CleanArchitecture/CleanArchitecture.Domain/Entities/Categoria.cs
@@ -1,9 +1,35 @@
-﻿namespace CleanArchitecture.Domain.Entities;
+﻿using CleanArchitecture.Domain.Validations;
 
-public class Categoria
+namespace CleanArchitecture.Domain.Entities;
+
+public sealed class Categoria : Entity
 {
-    public int Id { get; set; }
-    public string Nome { get; set; }
+    public Categoria(string nome)
+    {
+        ValidarDominio(nome);
+    }
 
-    public ICollection<Produto> Produtos { get; set; }
+    public Categoria(int id, string nome)
+    {
+        DomainExceptionValidation.When(id < 0, "Id inválido!");
+        Id = id;
+        ValidarDominio(nome);
+    }
+
+    public string Nome { get; private set; }
+
+    public ICollection<Produto> Produtos { get; private set; }
+
+    public void Atualizar(string nome)
+    {
+        ValidarDominio(nome);
+    }
+
+    private void ValidarDominio(string nome)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(nome), "Nome inválido! Nome é obrigatório.");
+        DomainExceptionValidation.When(nome.Length < 3, "Nome inválido! É necessário pelo pelo menos 3 caracteres.");
+
+        Nome = nome;
+    }
 }
