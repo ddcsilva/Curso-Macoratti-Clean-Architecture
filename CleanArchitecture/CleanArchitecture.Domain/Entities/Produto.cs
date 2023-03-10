12d35cb452e14171a7bbfe8d7efa3ae1731eb1d4
Danilo Silva <danilo.silva@msn.com>
2023-03-10 14:56:28 -0300

    Modelo Anëmico

---
+++ CleanArchitecture/CleanArchitecture.Domain/Entities/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace CleanArchitecture.Domain.Entities;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public int Estoque { get; set; }
+    public string Imagem { get; set; }
+
+    public int CategoriaId { get; set; }
+    public Categoria Categoria { get; set; }
+}
