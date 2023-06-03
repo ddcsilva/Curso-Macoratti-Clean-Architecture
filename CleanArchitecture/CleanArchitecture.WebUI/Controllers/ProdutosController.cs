636b711e21cd995de5fb03e147295b420b4b0fcb
Danilo <danilo.silva@msn.com>
2023-06-03 12:18:12 -0300

    Cria um novo produto

--- CleanArchitecture/CleanArchitecture.WebUI/Controllers/ProdutosController.cs
+++ CleanArchitecture/CleanArchitecture.WebUI/Controllers/ProdutosController.cs
@@ -1,15 +1,19 @@
+using CleanArchitecture.Application.DTOs;
 using CleanArchitecture.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArchitecture.WebUI.Controllers;
 
 public class ProdutosController : Controller
 {
     private readonly IProdutoService _produtoService;
+    private readonly ICategoriaService _categoriaService;
 
-    public ProdutosController(IProdutoService produtoService)
+    public ProdutosController(IProdutoService produtoService, ICategoriaService categoriaService)
     {
         _produtoService = produtoService;
+        _categoriaService = categoriaService;
     }
 
     [HttpGet]
@@ -17,5 +21,24 @@ public class ProdutosController : Controller
     {
         var produtos = await _produtoService.ObterProdutosAsync();
         return View(produtos);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Criar()
+    {
+        ViewBag.Categorias = new SelectList(await _categoriaService.ObterCategoriasAsync(), "Id", "Nome");
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Criar(ProdutoDTO produtoDTO)
+    {
+        if (ModelState.IsValid)
+        {
+            await _produtoService.AdicionarAsync(produtoDTO);
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(produtoDTO);
     }
 }
