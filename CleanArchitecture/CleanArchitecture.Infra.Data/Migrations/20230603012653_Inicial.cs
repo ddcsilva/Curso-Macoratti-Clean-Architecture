a2541f089a650aa3666d7f17d666f455e1baf2aa
Danilo <danilo.silva@msn.com>
2023-06-02 22:35:48 -0300

    Aplicando o Migrations do EF Core

---
+++ CleanArchitecture/CleanArchitecture.Infra.Data/Migrations/20230603012653_Inicial.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecture.Infra.Data.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false),
+                    Imagem = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "Id", "Nome" },
+                values: new object[] { 1, "Material Escolar" });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "Id", "Nome" },
+                values: new object[] { 2, "Eletrônicos" });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "Id", "Nome" },
+                values: new object[] { 3, "Acessórios" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Infra.Data/Migrations/20230603012859_SeedProdutos.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecture.Infra.Data.Migrations
+{
+    public partial class SeedProdutos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, Estoque, Imagem, CategoriaId) VALUES('Caderno', 'Caderno espiral 100 folhas', 7.45, 50, 'caderno.jpg', 1)");
+            migrationBuilder.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, Estoque, Imagem, CategoriaId) VALUES('Estojo', 'Estojo escolar cinza', 5.98, 120, 'estojo.jpg', 1)");
+            migrationBuilder.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, Estoque, Imagem, CategoriaId) VALUES('Borracha', 'Borracha branca pequena', 3.75, 80, 'borracha.jpg', 1)");
+            migrationBuilder.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, Estoque, Imagem, CategoriaId) VALUES('Apontador', 'Apontador com depósito', 2.99, 130, 'apontador.jpg', 1)");
+            migrationBuilder.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, Estoque, Imagem, CategoriaId) VALUES('Caneta', 'Caneta esferográfica azul', 1.99, 150, 'caneta.jpg', 1)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Produtos");
+        }
+    }
+}
